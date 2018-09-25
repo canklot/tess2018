e6a85ea3c83b8a1ed61ab84476d30e8b8282424a
canklot <canklot@github.com>
2018-09-25 17:14:47 +0300

    Dizinler değişkenlere atandı

--- oto.bat
+++ oto.bat
@@ -1,15 +1,19 @@
-cd C:\Users\ali\Desktop\tesseract-script-master\nir
-nircmd.exe savescreenshot "C:\Users\ali\Desktop\tesseract-script-master\scrnsht.png"
-cd C:\Users\ali\Desktop\tesseract-script-master\
+SET chromeDir=awfwaf
+cd nir
+nircmd.exe savescreenshot scrnsht.png
 magick scrnsht.png -crop 330x70+34+126 cropped.jpg
 magick convert cropped.jpg -negate -resize 150%% negativeres.jpg
 magick convert negativeres.jpg -colorspace gray gray.jpg
 magick convert gray.jpg -colorspace gray -threshold 25%% out.png
-cd C:\Users\ali\Desktop\tesseract-script-master\tess
-tesseract.exe C:\Users\ali\Desktop\tesseract-script-master\out.png C:\Users\ali\Desktop\tesseract-script-master\out
-cd C:\Users\ali\Desktop\tesseract-script-master
+SET nirDir=%cd%
+echo %nirDir%
+cd..
+SET mainDir=%cd%
+cd tess
+tesseract.exe %nirDir%\out.png %mainDir%\out
+cd..
 pyscript.py
 SET /p kelime=<srch.txt
-cd C:\Program Files (x86)\Google\Chrome\Application\
+pushd C:\Program Files (x86)\Google\Chrome\Application\
 chrome.exe https://www.google.com.tr/search?q=%kelime%
-cd C:\Users\ali\Desktop\tesseract-script-master
+popd
