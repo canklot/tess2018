36460a751a99fee4a068b52eaba6ebd59e8ada04
canklot <canklot@github.com>
2018-09-27 18:27:18 +0300

    Şıklar satır sayısına göre croplanıp teseracta yollandı ama tesseract okumuyor

--- oto.bat
+++ oto.bat
@@ -16,7 +16,17 @@ pyscript.py
 SET /p kelime=<srch.txt
 SET /p linecounter=<linecounter.txt
 cd nir
-IF %linecounter%==3 (magick scrnsht.png -crop 300x40+30+320 choiceA.jpg)
+IF %linecounter%==3 (magick scrnsht.png -crop 300x40+30+320 choiceA.png)
+IF %linecounter%==3 (magick scrnsht.png -crop 300x40+30+410 choiceB.png)
+IF %linecounter%==3 (magick scrnsht.png -crop 300x40+30+490 choiceC.png)
+IF %linecounter%==2 (magick scrnsht.png -crop 300x40+30+290 choiceA.png)
+IF %linecounter%==2 (magick scrnsht.png -crop 300x40+30+380 choiceB.png)
+IF %linecounter%==2 (magick scrnsht.png -crop 300x40+30+465 choiceC.png)
+cd..
+cd tess
+tesseract.exe %nirDir%\choiceA.png %mainDir%\choiceA
+tesseract.exe %nirDir%\choiceB.png %mainDir%\choiceB
+tesseract.exe %nirDir%\choiceC.png %mainDir%\choiceC
 cd..
 pushd C:\Program Files (x86)\Google\Chrome\Application\
 chrome.exe https://www.google.com.tr/search?q=%kelime%
