ae0fcf4220e65917bc75e7bf91b191d3e53ca80a
canklot <canklot@github.com>
2018-09-29 13:29:46 +0300

    şıklar soru ile beraber aranıyor

--- oto2.bat
+++ oto2.bat
@@ -3,6 +3,11 @@ tesseract.exe %nirDir%\outchoiceA.png %mainDir%\choiceA
 tesseract.exe %nirDir%\outchoiceB.png %mainDir%\choiceB
 tesseract.exe %nirDir%\outchoiceC.png %mainDir%\choiceC
 cd..
+SET /p choiceA=<choiceA.txt
+SET /p choiceB=<choiceB.txt
+SET /p choiceC=<choiceC.txt
 pushd C:\Program Files (x86)\Google\Chrome\Application\
-chrome.exe https://www.google.com.tr/search?q=%kelime%
+chrome.exe https://www.google.com.tr/search?q=%kelime%+%choiceA%
+chrome.exe https://www.google.com.tr/search?q=%kelime%+%choiceB%
+chrome.exe https://www.google.com.tr/search?q=%kelime%+%choiceC%
 popd
