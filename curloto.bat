5642e0d82385bcb8f7996c6c109651b59fe44be5
canklot <canklot@github.com>
2018-09-29 14:08:32 +0300

    curl kısmen ayarlandı

--- curloto.bat
+++ curloto.bat
@@ -1,4 +1,10 @@
 cd curl
 cd bin
-SET "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3"
-curl.exe 
+SET userAgent='Mozilla/5.0'
+SET kelime=asd
+SET choiceA=zxc
+SET query=https://www.google.com.tr/search?q=
+SEt not=yorum yapmayi bilmiyorum test amacli degişkenler ile oynadim düzelt
+curl.exe -A %userAgent% -o result.html "%query%%kelime%+%choiceA%"
+cd..
+cd..
